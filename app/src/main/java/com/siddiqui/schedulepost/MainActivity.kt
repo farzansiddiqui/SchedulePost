8fc4a9dcd7f27d7d1b392a28d193281c719079a8
@farzan.siddiqui <farzan.siddiqui@hexnbit.com>
2023-10-21 02:26:02 +0530

    file update

--- app/src/main/java/com/siddiqui/schedulepost/MainActivity.kt
+++ app/src/main/java/com/siddiqui/schedulepost/MainActivity.kt
@@ -1,24 +1,46 @@
 package com.siddiqui.schedulepost
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.siddiqui.schedulepost.adapter.GridViewAdapter
 import com.siddiqui.schedulepost.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val imageUris = mutableListOf<Uri?>(null)
+    private lateinit var adapter: GridViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val picker = PhotoPicker(this){uri ->
+        val picker = PhotoPicker(this) { uri ->
             if (uri != null) {
-               // binding.postImageView.setImageURI(uri)
+                // binding.postImageView.setImageURI(uri)
             } else {
                 Toast.makeText(this, "No Image Select", Toast.LENGTH_SHORT).show()
             }
         }
+        adapter = GridViewAdapter(this, imageUris)
+        binding.gridView.adapter = adapter
+
+        val photoPicker = PhotoPicker(this) { uri ->
+            imageUris[imageUris.indexOf(null)] = uri
+            Log.d("TAG", "Image select")
+            adapter.notifyDataSetChanged()
+        }
+
+        binding.gridView.setOnItemClickListener { _, _, position, _ ->
+            if (imageUris[position] == null) {
+                photoPicker.pickMedia()
+            }
+        }
+
 
     }
 
--- app/src/main/java/com/siddiqui/schedulepost/adapter/GridViewAdapter.kt
+++ app/src/main/java/com/siddiqui/schedulepost/adapter/GridViewAdapter.kt
@@ -1,11 +1,13 @@
 package com.siddiqui.schedulepost.adapter
 
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import com.siddiqui.schedulepost.R
 import com.siddiqui.schedulepost.model.GridImageView
@@ -13,20 +15,17 @@ import com.siddiqui.schedulepost.viewmodel.GridViewModel
 
 class GridViewAdapter(
     private val context: Context,
-    private val data: LiveData<ArrayList<GridImageView>>,
-    private var arrayList: ArrayList<GridViewModel>
+    private val imageUrs:MutableList<Uri?>
 ) : BaseAdapter() {
     private var layoutInflater: LayoutInflater? = null
 
     // below method is use to return the count of course list
     override fun getCount(): Int {
-        return arrayList.size
+        return  imageUrs.size
     }
 
-    // below function is use to return the item of grid view.
-    override fun getItem(position: Int): GridImageView {
-        return data.value?.get(position)
-    }
+    // below function is use to return the item of grid view)
+    override fun getItem(position: Int) = imageUrs[position]
 
     // below function is use to return item id of grid view.
     override fun getItemId(position: Int): Long {
@@ -43,9 +42,30 @@ class GridViewAdapter(
         if (convertView == null) {
             view = layoutInflater?.inflate(R.layout.add_image_grid, parent, false)
         }
-        val gridViewModel = getItem(position)
-        val setImageVIew = view?.findViewById<ImageView>(R.id.set_imageView)
-        setImageVIew?.setImageURI(gridViewModel.)
+
+
+        val setImageView = view?.findViewById<ImageView>(R.id.set_imageView)
+        val addImageView = view?.findViewById<ImageView>(R.id.add_imageView)
+        val clearButton = view?.findViewById<ImageView>(R.id.clear_imageView)
+        val addPostTextView = view?.findViewById<TextView>(R.id.textView)
+        val uri = getItem(position)
+
+        if (uri != null){
+            setImageView?.setImageURI(uri)
+            clearButton?.visibility = View.VISIBLE
+            addImageView?.visibility = View.GONE
+            addPostTextView?.visibility = View.GONE
+
+        }else {
+            setImageView?.setImageURI(null)
+            clearButton?.visibility = View.GONE
+
+        }
+       /* addImageView?.setOnClickListener {
+           imageUris.add(position, null)
+            notifyDataSetChanged()
+        }*/
+
 
         return view!!
     }
